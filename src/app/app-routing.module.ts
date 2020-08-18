b650429e1109224724f797603708c25b13623625
Julian Sapundzhiev <julian_s@abv.bg>
2020-08-18 17:05:11 +0300

    Initial Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StateMachineComponent } from './state-machine/state-machine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'state-machine/:id', component: StateMachineComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
